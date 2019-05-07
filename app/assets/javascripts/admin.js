96adf2e0389a0b14e8916de16a9c7e72233796c1
Bill Hewitt <billhewitt22@googlemail.com>
2019-05-07 17:56:02 +0100

    ready

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -348,3 +348,16 @@ File: Main Js File
 
 // Waves Effect
 Waves.init();
+
+
+$(function(){
+  var hash = window.location.hash;
+  hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+
+  $('.nav-tabs a').click(function (e) {
+    $(this).tab('show');
+    var scrollmem = $('body').scrollTop() || $('html').scrollTop();
+    window.location.hash = this.hash;
+    $('html,body').scrollTop(scrollmem);
+  });
+});
