a290d9884de25a35fc76cce9f731b4cf60e9367b
Bill Hewitt <billhewitt22@googlemail.com>
2019-05-10 02:49:43 +0100

    ready

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -446,4 +446,10 @@ $('.scroller-left').click(function() {
     });
 });  
 
-  
+$('#btnClick').on('click',function(){
+    if($('#1').css('display')!='none'){
+    $('#2').html('Here is my dynamic content').show().siblings('div').hide();
+    }else if($('#2').css('display')!='none'){
+        $('#1').show().siblings('div').hide();
+    }
+});  
